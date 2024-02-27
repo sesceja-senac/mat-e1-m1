b79a01dc3820b4317082c2ad14a89f587f94a816
lui <luiz.pb@cryptolab.net>
2024-02-27 12:51:55 -0300

    reset repo

--- história_dos_numeros/assets/js/geral.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function(){ 
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-
-    $('.preloader').fadeOut(300);
-})
-
